da8d9c32bd8eecf4b5eed3e91b29577191780790
BangBarMau <74715623+BangBarMau@users.noreply.github.com>
2021-10-26 13:04:13 +0800

    10/26 1:03PM

--- Library.py
+++ Library.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, helpers
+from flask_restful import Api, Resource
+from flask.helpers import make_response
 
 libsearch = Flask(__name__)
+searchlib = Api(libsearch)
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -18,51 +21,77 @@ swagger = Swagger(libsearch)
 run_with_ngrok(libsearch)
 
 
-@libsearch.route('/')
-@libsearch.route('/books', methods=['GET'])
-def showBooks():
-    books = session.query(Book).all()
-    return render_template("books.html", books=books)
+class Books(Resource):
+    def get(self):
+        books = session.query(Book).all()
+        headers = {'Content-Type': 'text/html'}
+        return make_response(render_template("books.html", books=books), 200, headers)
 
+class addBooks(Resource):
+    def get(self):
+        headers = {'Content-Type': 'text/html'}
+        return make_response(render_template("newBook.html"), 200, headers)
 
-@libsearch.route('/books/new/', methods=['GET', 'POST'])
-def newBook():
-    if request.method == 'POST':
+    def post(self):
         newBook = Book(title=request.form['name'], author=request.form['author'], genre=request.form['genre'])
         session.add(newBook)
         session.commit()
-        return redirect(url_for('showBooks'))
-    else:
-        return render_template('newBook.html')
+        return redirect(url_for('books'))
+
+class editBook(Resource):
+    def get(self, book_id):
+        editedBook = session.query(Book).filter_by(id=book_id).one()
+        headers = {'Content-Type': 'text/html'}
+        return make_response(render_template("editBook.html", book=editedBook), 200, headers)
+
+    def post(self, book_id):
+        editedBook = session.query(Book).filter_by(id=book_id).one()
+        if request.method == 'POST' or request.method == 'PUT':
+            if request.form['name']:
+                editedBook.title = request.form['name']
+            if request.form['author']:
+                editedBook.author = request.form['author']
+            if request.form['genre']:
+                editedBook.genre = request.form['genre']
+                session.commit()
+                return redirect(url_for('books'))
+
+    def put(self, book_id):
+        editedBook = session.query(Book).filter_by(id=book_id).one()
+        if request.method == 'POST' or request.method == 'PUT':
+            if request.form['name']:
+                editedBook.title = request.form['name']
+            if request.form['author']:
+                editedBook.author = request.form['author']
+            if request.form['genre']:
+                editedBook.genre = request.form['genre']
+                session.commit()
+                return redirect(url_for('books'))
 
 
-@libsearch.route("/books/<int:book_id>/edit/", methods=['GET', 'PUT', 'POST'])
-def editBook(book_id):
+class deleteBook(Resource):
+    def get(self, book_id):
+        editedBook = session.query(Book).filter_by(id=book_id).one()
+        headers = {'Content-Type': 'text/html'}
+        return make_response(render_template("deleteBook.html", book=editedBook), 200, headers)
 
-    editedBook = session.query(Book).filter_by(id=book_id).one()
-    if request.method == 'POST' or request.method == 'PUT':
-        if request.form['name']:
-            editedBook.title = request.form['name']
-        if request.form['author']:
-            editedBook.author = request.form['author']
-        if request.form['genre']:
-            editedBook.genre = request.form['genre']
-            session.commit()
-            return redirect(url_for('showBooks'))
-    else:
-        return render_template('editBook.html', book=editedBook)
-
-
-@libsearch.route('/books/<int:book_id>/delete/', methods=['GET', 'POST', 'DELETE'])
-def deleteBook(book_id):
-    bookToDelete = session.query(Book).filter_by(id=book_id).one()
-    if request.method == 'POST' or request.method == 'DELETE':
+    def post(self, book_id):
+        bookToDelete = session.query(Book).filter_by(id=book_id).one()
         session.delete(bookToDelete)
         session.commit()
-        return redirect(url_for('showBooks', book_id=book_id))
-    else:
-        return render_template('deleteBook.html', book=bookToDelete)
+        return redirect(url_for('books'))
 
+    def delete(self, book_id):
+        bookToDelete = session.query(Book).filter_by(id=book_id).one()
+        session.delete(bookToDelete)
+        session.commit()
+        return redirect(url_for('books'))
+
+
+searchlib.add_resource(Books, '/', '/books/')
+searchlib.add_resource(addBooks, '/books/new/')
+searchlib.add_resource(editBook, '/books/<book_id>/edit/')
+searchlib.add_resource(deleteBook, '/books/<book_id>/delete/')
 
 if __name__ == '__main__':
     libsearch.debug = True
